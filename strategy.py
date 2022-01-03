d66f21d6281b781e3eb8865ab3d2512870fe6283
Na1k <nick.kramer9926@gmail.com>
2022-01-03 13:10:35 +0100

    Kraken KryptoBot v1.0

--- strategy.py
+++ strategy.py
@@ -61,36 +61,62 @@ def printOhlcImage(df, name):
     fig.write_image("figures\\"+name)
     # fig.show() #output in browser
 
-
 def get_RSI(df, per):
     return talib.RSI(df["close"], timeperiod=per)
-
 
 def get_MACD(df, fp, slowp, sigp):
     macd, macdsignal, macdhist = talib.MACD(
         df["close"], fastperiod=fp, slowperiod=slowp, signalperiod=sigp)
     return macd, macdsignal, macdhist
 
-
 def get_BBANDS(df, tp, nbdup, nbddn, mt):
     upperband, middleband, lowerband = talib.BBANDS(
         df["close"], timeperiod=tp, nbdevup=nbdup, nbdevdn=nbddn, matype=mt)
     return upperband, middleband, lowerband
 
-
 def get_percentB(df):
     return ((df["close"]-df["BBlower"])/(df["BBupper"] - df["BBlower"]))
 
-def buy(df):
-    print("KAUFEN", datetime.now(), "Preis", df["close"].values[0])
-    return (df["close"].values[0] - 150)
+def buy(df, info):
+    try:
+        balance = kraken.query_private("Balance")
+        balance = float(balance['result']['ZEUR'])
+        ETHval = df['close'].values[0]
+        info['volume'] = 0.95*(balance/ETHval)
+        kraken.query_private('AddOrder',
+                            {'pair': 'ETHEUR',
+                            'type': 'buy',
+                            'ordertype': 'market',
+                            'volume': info['volume']
+                            })
+        info['stoploss'] = df["close"].values[0] - 75
+        info['open_position'] = True
+        print("KAUFEN", datetime.now(), "Preis", df["close"].values[0], flush=True)
+    except (Exception) as e:
+        info['open_position'] = False
+        print(e, flush=True)
+    print(info, flush=True)
+    return info
 
-def sell(df):
-    print("VERKAUFEN", datetime.now(), "Preis", df["close"].values[0])
-    return
+def sell(df, info):
+    try:
+        kraken.query_private('AddOrder',
+                            {'pair': 'ETHEUR',
+                            'type': 'sell',
+                            'ordertype': 'market',
+                            'volume': info['volume']
+                            })
+        info['open_position'] = False
+        info['stoploss'] = 0
+        info['volume'] = 0
+        print("VERKAUFEN", datetime.now(), "Preis", df["close"].values[0], flush=True)
+    except (Exception) as e:
+        print(e, flush=True)
+    print(info, flush=True)
+    return info
 
 def asc_stoploss(df, info):
-    stoploss_now = df["close"].values[0] - 150
+    stoploss_now = df["close"].values[0] - 50
     if(info['stoploss'] < stoploss_now):
         info['stoploss'] = stoploss_now
     return info
@@ -100,36 +126,32 @@ def strat(df, info):
     time_df = df["time_end"].values[0]
     time_now = np.datetime64(datetime.now())
     if(not(time_now < time_df) or (not(time_now > (time_df - np.timedelta64(5,"m"))))):
-        print("time error")
+        time.sleep(5)
         return info
     
-    print(info)
     if(not(info['open_position'])):     #Buy Descision
         if((df["RSI"].values < 30) & (df["percent_b"].values < 0)):
-            info['stoploss'] = buy(df)
+            info = buy(df, info)
             df2 = df[['close', 'time_end']].copy()
             df2["desc"] = ["b"]
             df2.to_sql('strat', engine, if_exists='append', index=False)
-            info['open_position'] = True
             return info
     elif(info['open_position']):
         if(df['close'].values[0] < info['stoploss']):   #Stop-Loss Descision
-            sell(df)
+            info = sell(df, info)
             df2 = df[['close', 'time_end']].copy()
             df2["desc"] = ["sl"]
             df2.to_sql('strat', engine, if_exists='append', index=False)
-            info['open_position'] = False
             time.sleep(900)
             return info
         else:
             info = asc_stoploss(df, info)       #check for ascending Stop-Loss if Stop-Loss didn't trigger
         
         if((df["RSI"].values > 70) & (df["percent_b"].values > 1)):     #Selling Descision
-            sell(df)
+            info = sell(df, info)
             df2 = df[['close', 'time_end']].copy()
             df2["desc"] = ["s"]
             df2.to_sql('strat', engine, if_exists='append', index=False)
-            info['open_position'] = False
             return info
     return info
 
@@ -139,7 +161,8 @@ engstr = 'mysql+pymysql://root:root@localhost/kryptotrading?charset=utf8mb4'
 
 engine = sqlalchemy.create_engine(engstr)
 trade = {'open_position':False,
-         'stoploss':0}
+         'stoploss':0,
+         'volume':0}
 
 while True:
     try:
@@ -154,5 +177,5 @@ while True:
         printOhlcImage(df, "ohlc.png")
         trade = strat(df.iloc[-1:], trade)
     except (Exception, KeyboardInterrupt) as e:
-        print(e)
+        print(e, flush=True)
     time.sleep(1.)
